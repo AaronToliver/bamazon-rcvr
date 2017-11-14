74aec955b7f06e9a7a1d4559acc8fb601f51643c
Aaron Toliver <aaronaugustine85@gmail.com>
2017-11-14 13:24:19 -0500

    recovered commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# bamazon
+
+## Synopsis
+
+This is a Amazon like product that has e-commerce type ordering with an inventory on the back-end
+
+## Code Example
+<ul><h3><b>
+To run program: node bamazonCLI.js
+</b></h3></ul>
+<li>Customer Access</li>
+<li>Manager Access</li>
+<li>Supervisor Access</li>
+
+## Motivation
+
+To learn how to send and receive information with npm package 'mysql'
+
+## Installation
+
+Access with running package.json file
+
+
+
+
---
+++ bamazonCustomer.js
@@ -0,0 +1,71 @@
+var inquirer = require('inquirer');
+var mysql = require('mysql');
+var AsciiTable = require('ascii-table');
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'password',
+    database : 'bamazon'
+});
+   
+function bamazonCustomer() {  
+inquirer
+    .prompt([
+        {
+         type: 'input',
+         name: 'product',
+         message: 'What would you like to buy (Product ID)?'
+        }
+    ]).then(function(purchase) {
+            inquirer
+                .prompt([
+                    {
+                        type: 'input',
+                        name: 'quantity',
+                        message: 'How many would you like to buy?'
+                    }
+                ]).then(function(ordered){
+                    var qtyOrdered = parseFloat(ordered.quantity);
+                    console.log(`Ordered Quantity: ${qtyOrdered}`);
+                    connection.connect();
+                    
+                    connection.query(`SELECT * FROM bamazon.products where ?`,
+                    {
+                        item_id: purchase.product
+                    },
+                        function (error, results, fields) {
+                    if (error) throw error;
+                        var qtyAvail = results[0].stock_quantity;
+                        var currentSales = results[0].product_sales;
+                        var salePrice = results[0].price;
+                        var currentSold = results[0].product_sold;
+                        if(qtyAvail < qtyOrdered) {
+                            console.log('Insufficient Quantity available!!');
+                            connection.end();
+                            return;
+                        } else {
+                            var newStockQty = qtyAvail - qtyOrdered;
+                            var qtySold = currentSold + qtyOrdered;
+                            var newSales = currentSales + (qtyOrdered * salePrice);
+                            console.log('Your order has been placed!!');
+                                connection.query(`UPDATE products SET ? WHERE ?`,
+                                    [{
+                                        stock_quantity: newStockQty,
+                                        product_sales: newSales,
+                                        product_sold: qtySold
+                                    },
+                                    {
+                                        item_id: purchase.product
+                                    }], function (error, results, fields) {
+                                    if (error) throw error;
+                                    connection.end();
+                                    return;
+                                });
+                        } 
+                    });
+                });
+    });
+}
+
+module.exports = bamazonCustomer;
